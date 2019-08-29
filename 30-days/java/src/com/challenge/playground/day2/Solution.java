e890efb284c91cc52d36e36f3138f297ee5b4f6e
Randolfo Castillo <randolfo.castillo@globant.com>
2019-08-28 23:54:29 -0300

    Code Day 2

---
+++ 30-days/java/src/com/challenge/playground/day2/Solution.java
@@ -0,0 +1,28 @@
+package com.challenge.playground.day2;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N >= 1 && N <= 100) {
+            if (N % 2 == 0) {
+                if (N <= 5) {
+                    System.out.println("Not Weird");
+                } else if (N <= 20) {
+                    System.out.println("Weird");
+                } else {
+                    System.out.println("Not Weird");
+                }
+            } else {
+                System.out.println("Weird");
+            }
+        }
+        scanner.close();
+    }
+}
