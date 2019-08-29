19209faa003b95d8cdd119e5b6e63ccc7d57ce51
Randolfo Castillo <randolfo.castillo@globant.com>
2019-08-28 23:19:24 -0300

    Code Day 0

---
+++ 30-days/java/src/com/challenge/playground/day0/Solution.java
@@ -0,0 +1,9 @@
+package com.challenge.playground.day0;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
